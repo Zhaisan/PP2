d77db4a861039b4cc9f7517aefa5717189ccffd3
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-01-31 23:14:29 +0600

    qw

---
+++ informatics/lists/a.py
@@ -0,0 +1,8 @@
+#e = int(input())
+
+#a = [0 for i in range(e)]
+#if i%2==0:
+    #print(a)
+a = input().split()
+for i in range(0, len(a), 2):
+    print(a[i])
---
+++ informatics/lists/c.py
@@ -0,0 +1,7 @@
+list = input().split()
+cnt=0
+for i in list:
+    if int(i)>0: 
+        cnt+=1 
+
+print(cnt)
